68c49f7f301b1b65d3812d1d9c5be2fdde795ec4
Tom Sherman <tompsherman@gmail.com>
2020-09-08 17:29:21 -0400

    app, posts, post, like section

--- src/App.js
+++ src/App.js
@@ -5,13 +5,26 @@
 */
 
 // Import the state hook
-import React from 'react';
+import React, {useState} from 'react';
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
+
+import Search from './components/SearchBar/SearchBar'
+import Post from './components/Posts/Post'
+import PostHeader from './components/Posts/PostHeader'
+import Posts from './components/Posts/Posts'
+import LikeSection from './components/Posts/LikeSection'
+
+
 // Import the dummyData
+import dummyData from './dummy-data'
 import './App.css';
 
 const App = () => {
-  // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
+  // Create a state called `posts` to hold the array of post objects, 
+  
+  const [posts, setPosts] = useState(dummyData)
+  
+  // **initializing to dummyData**.
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
 
@@ -27,12 +40,18 @@ const App = () => {
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
+
+    setPosts(posts.map((item => {
+      return item.id === postId ? {...item, likes: item.likes+1} : item    })))
+
   };
 
   return (
     <div className='App'>
       {/* Add SearchBar and Posts here to render them */}
+      <Search />
       {/* Check the implementation of each component, to see what props they require, if any! */}
+      <Posts likePost={likePost} posts={posts}/>
     </div>
   );
 };
--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import Comment from './Comment';
-import './Comments.css';
+import React from "react";
+import Comment from "./Comment";
+import "./Comments.css";
 
-const Comments = props => {
+const Comments = (props) => {
   // 🔥 Make sure the parent of Comments is passing the right props!
   const { comments } = props;
 
   return (
-    <div>
+   
       {/* map through the comments prop and render a Comment for every piece of data */}
+      <div>
+
+      {comments.map((item => {
+        return <Comment comment={item.text} />;
+      }))}
+
     </div>
   );
 };
